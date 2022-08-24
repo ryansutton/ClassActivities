28a17ac9c2b1ff6f82f47a8a2d5bb8281604c75a
ryansutton <ryanchristiansutton@gmail.com>
2022-08-23 21:47:26 -0500

    JS Variables and Primitives Parts 1 and 2

---
+++ Section02/2.2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,40 @@
+// create a constant variable (const)
+
+// create a variable that can be reassigned (let)
+
+// create three variables and assign them values of different data types (=)
+
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+
+// change the value referenced by a variable (dynamic typing)
+
+// print the type of the variable you just changed (typeof)
+
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+
+const team = "Jayhawks";
+let num = 3;
+let a = true;
+console.log(typeof team);
+console.log(typeof num);
+num = "three";
+console.log(typeof num);
+const name = "Ryan";
+let language = "Javascript";
+let number = 2;
+let status = false;
+
+console.log(
+  "Hello, my name is " +
+    name +
+    ", and I am learning " +
+    language +
+    ". I have been practicing for only " +
+    number +
+    " weeks, so my status as a master is: " +
+    status +
+    "."
+);
---
+++ Section02/2.2/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,35 @@
+// create a variable called "name" that references a string
+const name = "Ryan";
+// create a variable called "favoriteSong" that references a string
+let favoriteSong = "I'll Be";
+// create a variable called "wage" that references a number
+let wage = 50000;
+// create a variable called "age" that references a number
+let age = 32;
+// create a variable called "onlyChild" that references a boolean
+let onlyChild = false;
+// create a variable called "satisfied" that references a boolean
+let satisfied = false;
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+let favoriteThing = "dog";
+console.log(typeof favoriteThing);
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong = "Brand New";
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = 3;
+console.log(typeof favoriteThing);
+// Happy Birthday!
+// Reassign "age" to a new value
+age = 33;
+// You got a big raise at work.
+// Reassign "wage" to a new value
+wage = 100000;
+// Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisfied = true;
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
+console.log("My name is " + name + " and I am " + age + " years old.");
+console.log(`My name is ${name} and I am ${age} years old.`);
