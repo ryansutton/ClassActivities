4690954a3cac64ff00288f9126c6fe58928a55bd
ryansutton <ryanchristiansutton@gmail.com>
2022-09-06 21:49:34 -0500

    4.1 Class Activities

---
+++ Section04/4.1/4.1.1-SelectingDOMElements.js
@@ -0,0 +1,9 @@
+// select all unordered list (ul) elements
+console.log(document.querySelectorAll("ul"));
+// select all elements with the class "class-week"
+console.log(document.querySelectorAll(".class-week"));
+// select all elements with the class "nav-bar"
+console.log(document.querySelectorAll(".nav-bar"));
+// select the element with the id "dog-picture" and save it to a variable
+const dogPicture = document.querySelector("#dog-picture");
+console.log(dogPicture);
